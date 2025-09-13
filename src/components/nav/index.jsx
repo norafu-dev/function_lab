d167eadd181165821f7680ba252ca4ebe6c0d934
norafu-dev <norafudev@gmail.com>
2025-09-13 21:21:26 +0900

    hide navbar in studio page

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,8 +1,12 @@
 "use client";
 
 import LaptopNav from "./LaptopNav";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
+  const pathname = usePathname();
+  const isStudio = pathname.includes("/studio");
+  if (isStudio) return null;
   return (
     <div className="hidden md:block">
       <LaptopNav />
