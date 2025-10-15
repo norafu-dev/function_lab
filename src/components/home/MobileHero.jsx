399ae8585388c64263dfff67d3d5612446fc1cab
norafu-dev <norafudev@gmail.com>
2025-10-16 00:22:28 +0900

    adjust hero text final position

--- src/components/home/MobileHero.jsx
+++ src/components/home/MobileHero.jsx
@@ -41,8 +41,8 @@ const MobileHero = ({ src, poster, info }) => {
     )
       .fromTo(
         ".mobile-hero-info",
-        { y: "85vh" },
-        { y: "78vh", ease: "power1.inOut", duration: 1.2 },
+        { y: "80vh" },
+        { y: "75vh", ease: "power1.inOut", duration: 1.2 },
         "<0.1"
       )
       .to(
